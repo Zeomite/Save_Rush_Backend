c3a21fe93fc0c94e1b40f649ef741b998fdadcaa
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2025-01-27 14:16:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = require('./src/server.js');
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req ,res) => {
+    res.send("Server is Running")
+})
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
